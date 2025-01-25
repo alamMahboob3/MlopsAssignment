95c2bbb70741ca1e7f4f04dc7074b466e58d348e
alamMahboob3 <alam93.mahboo@gmail.com>
2025-01-25 17:19:23 +0400

    second commitsss

---
+++ train.py
@@ -0,0 +1,18 @@
+# train.py
+from sklearn.linear_model import LinearRegression
+from sklearn.datasets import load_diabetes
+import joblib
+
+# Load dataset
+data = load_diabetes()
+X, y = data.data, data.target
+print("Dataset loaded.")
+
+# Train model
+model = LinearRegression()
+model.fit(X, y)
+print("Model training complete.")
+
+# Save model
+joblib.dump(model, "model.pkl")
+print("Model training complete. Model saved as model.pkl.")
